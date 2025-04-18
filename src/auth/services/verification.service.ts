ba5c48eeceabe10582c0d62f6c092aaf3ad1711d
EdiGal91 <edigal91@gmail.com>
2025-04-18 03:52:45 +0300

    fix install uuid

--- src/auth/services/verification.service.ts
+++ src/auth/services/verification.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { v4 as uuidv4 } from 'uuid';
-import { User, UserDocument } from '../../users/schemas/user.schema';
+import { User } from '../../users/schemas/user.schema';
 import { UsersService } from '../../users/users.service';
 
 // Would typically be in its own schema file
