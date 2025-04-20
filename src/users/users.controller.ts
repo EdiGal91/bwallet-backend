37c5849a1b219515f77b4c24cc4955b049462574
EdiGal91 <edigal91@gmail.com>
2025-04-20 20:18:09 +0300

    remove get user by id

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -9,11 +9,6 @@ import { UpdateUserDto } from './dto/update-user.dto';
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Get(':id')
-  findOne(@Param('id') id: string): Promise<User> {
-    return this.usersService.findById(id);
-  }
-
   @Patch(':id')
   update(
     @Param('id') id: string,
